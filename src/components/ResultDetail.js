90e9a05bf1972485776c284654754983996a4730
Burak Dansık <bburakdnsk@gmail.com>
2025-06-13 06:25:02 +0300

    css edits

--- src/components/ResultDetail.js
+++ src/components/ResultDetail.js
@@ -3,12 +3,26 @@ import React from 'react'
 
 export default function ResultDetail({result}) {
   return (
-    <View>
-      <Image style ={{width:250,height:120}}source={result.image_url ?{uri:result.image_url}: null}/>
-      <Text>{result.name}</Text>
+    <View style={styles.container}>
+      <Image style ={styles.image}source={result.image_url ?{uri:result.image_url}: null}/>
+      <Text style={styles.names} >{result.name}</Text>
       <Text>{result.rating} Yıldızlı Restoran, {result.review_count} Adet Yorum </Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    marginLeft:15,
+  },
+  image:{
+    width:250,
+    height:120,
+    borderRadius:8,
+    marginBottom:5,
+  },
+  names:{
+    fontWeight:'bold',
+
+  }
+})
