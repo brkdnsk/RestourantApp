07c8dadf867f9c8420f94ac024d8c3de6eb8ac43
Burak Dansık <bburakdnsk@gmail.com>
2025-06-13 04:19:28 +0300

    Photos

--- src/components/ResultDetail.js
+++ src/components/ResultDetail.js
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,Image} from 'react-native'
 import React from 'react'
 
 export default function ResultDetail({result}) {
   return (
     <View>
+      <Image style ={{width:250,height:120}}source={{uri:result.image_url}}/>
       <Text>ResultDetail</Text>
     </View>
   )
