6451e9685e5eb424cc76500aea3cda239258f44c
Burak Dansık <bburakdnsk@gmail.com>
2025-06-14 03:58:38 +0300

    restaurant information

--- src/Screens/ResultsShowScreen.js
+++ src/Screens/ResultsShowScreen.js
@@ -1,10 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { useEffect, useState } from 'react';
+import yelp from '../Api/yelp';
 
-export default function ResultsShowScreen() {
+export default function ResultsShowScreen({ route }) {
+  const [result, setResult] = useState(null);
+  const id = route.params.id;
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    console.log(response.data);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text>{result.name}</Text>
+      <Text>{result.phone}</Text>
+      <FlatList
+        data={result.photos}
+        renderItem={({ item }) => {
+          return (
+            <Image style={{ width: 50, height: 50 }} source={{ uri: item }} />
+          );
+        }}
+      />
     </View>
   );
 }
