8ae7ac7d48aa70a179357d8b303498c780fb55e7
Burak Dansık <bburakdnsk@gmail.com>
2025-06-14 04:02:06 +0300

    On/Off Icon

--- src/Screens/ResultsShowScreen.js
+++ src/Screens/ResultsShowScreen.js
@@ -1,7 +1,9 @@
 import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { useEffect, useState } from 'react';
-import yelp from '../api/yelp';
+import yelp from '../Api/yelp';
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function ResultsShowScreen({ route }) {
   const [result, setResult] = useState(null);
@@ -24,6 +26,14 @@ export default function ResultsShowScreen({ route }) {
     <View>
       <Text style={styles.title}>{result.name}</Text>
       <Text style={styles.phone}>{result.phone}</Text>
+      <View style={styles.icon}>
+        {result.is_closed ? (
+          <AntDesign name="closecircleo" size={30} color="black" />
+        ) : (
+          <MaterialIcons name="delivery-dining" size={30} color="black" />
+        )}
+      </View>
+
       <FlatList
         data={result.photos}
         renderItem={({ item }) => {
@@ -48,5 +58,8 @@ const styles = StyleSheet.create({
   phone: {
     alignSelf: 'center',
     fontSize: 20,
+  },
+  icon: {
+    alignSelf: 'center',
   },
 });
